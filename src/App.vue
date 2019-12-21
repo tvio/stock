fc9b556e1e3c364dc5f7080d0e880112c9dc4c43
unknown <hajek@NBE7490-22070.sukl.cz>
2019-12-21 12:12:54 +0100

    html pro header

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,24 @@
 <template>
     <div class="container">
+        <app-header></app-header>
 <router-view></router-view>
     </div>
 </template>
 
 <script>
+import Header from './components/Header.vue';
 
     export default {
+        components: {
+            appHeader: Header
+        }
 
     }
 </script>
 
 <style>
+ body {
+     padding:30px;
+ }
 
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,40 @@
 <template>
-    <h1>The Header Component</h1>
+    <nav class="navbar navbar-dark bg-primary">
+        
+            <div class="navbar-header">
+                <router-link to="/" class="navbar-brand">Stock Trader</router-link>
+          
+
+            <div class="collapse navbar-collapse">
+                <ul class=" navbar-nav mr-auto">
+                  
+                    <router-link to="/portfolio" activeClass="nav-item active" tag="li"><a>Portfolio</a></router-link>
+                    <router-link to="/stocks" activeClass="active" tag="li"><a>Stocks</a></router-link>
+                    </ul>
+                <strong class="navbar-text navbar-right">Funds: </strong>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#">End Day</a></li>
+                    <li
+                            class="dropdown">
+                          
+                        <a
+                                href="#"
+                                class="dropdown-toggle"
+                                data-toggle="dropdown"
+                                role="button"
+                                aria-haspopup="true"
+                                aria-expanded="false">Save & Load <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Save Data</a></li>
+                            <li><a href="#">Load Data</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav>
 </template>
+
+<script>
+   
+</script>
